ff3dd34703b7a4413448ca41f08b8f710f43848e
DeWuwo <767238116@qq.com>
2022-04-16 11:00:15 +0800

    fix: rename to lower

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -8,15 +8,15 @@ class FileReader:
     outTitle = ['diff', 'section', 'example', 'anti-patterns', 'stat']
 
     @classmethod
-    def read_from_json(cls, fileAndroid, fileHonor: str):
+    def read_from_json(cls, file_android, file_honor: str):
         print('reading file...')
         try:
-            with open(fileHonor, encoding='utf-8') as h:
+            with open(file_honor, encoding='utf-8') as h:
                 honor = json.load(h, strict=False)
                 entities_honor = honor['variables']
                 cells_honor = honor['cells']
                 entities_stat_honor = honor["entityNum"]
-            with open(fileAndroid) as a:
+            with open(file_android) as a:
                 android = json.load(a, strict=False)
                 entities_aosp = android['variables']
                 cells_aosp = android['cells']
@@ -27,16 +27,16 @@ class FileReader:
             raise e
 
     @classmethod
-    def write_to_json(cls, outPath: str, section: list, mode):
-        os.makedirs(outPath, exist_ok=True)
-        with open(outPath + cls.outFile[mode], 'w+', encoding='utf-8') as o:
+    def write_to_json(cls, out_path: str, section: list, mode):
+        os.makedirs(out_path, exist_ok=True)
+        with open(out_path + cls.outFile[mode], 'w+', encoding='utf-8') as o:
             json.dump({cls.outTitle[mode]: section}, o, ensure_ascii=False, indent=4)
 
     @classmethod
-    def write_match_mode(cls, outPath: str, matchSet: List[Dict]):
-        for item in matchSet:
+    def write_match_mode(cls, out_path: str, match_set: List[Dict]):
+        for item in match_set:
             for mode in item:
-                mode_path = outPath + '/' + mode
+                mode_path = out_path + '/' + mode
                 os.makedirs(mode_path, exist_ok=True)
                 for index, exa in enumerate(item[mode]):
                     exa_path = mode_path + '/' + str(index)
