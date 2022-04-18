be4553818579e2819ee97473781084c8eb0dc16d
DeWuwo <767238116@qq.com>
2022-04-18 18:01:26 +0800

    feat: test blame

--- test/test.py
+++ test/test.py
@@ -2,5 +2,8 @@ class Test:
     def test1(self, a: int, b: int):
         pass
 
-    def test2(self, a: int, b: int):
-        return a + b
+    def test3(self, a: int, b: int):
+        return a * b
+
+    def test2(self, a: int, b: int, c: int):
+        return a + b + c
