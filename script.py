fe9acca8aac966595be57afa38d33493c048c539
DeWuwo <767238116@qq.com>
2022-08-30 17:52:12 +0800

    feat: 添加部分分析结果

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ import sys
 import time
 
 if __name__ == '__main__':
-    Script('D:\\Honor\\source_code\\utils\\bin').run_command()
+    Script('D:\\Honor\\source_code\\utils\\bin').run_honor_command()
     lineage = [('lineage-16.0', 'D:\\Honor\\match_res\\LineageOS\\base\\lineage-16.0'),
                ('lineage-17.1', 'D:\\Honor\\match_res\\LineageOS\\base\\lineage-17.1'),
                ('lineage-18.1', 'D:\\Honor\\match_res\\LineageOS\\base\\lineage-18.1'),
@@ -60,4 +60,4 @@ if __name__ == '__main__':
     # IntrusiveType().run_filter(honor_int)
 
     # latex表格数据格式847330820
-    ToLatex('E:\\2022ASE\\data.csv').to_latex()
+    # ToLatex('E:\\2022ASE\\data.csv').to_latex()
