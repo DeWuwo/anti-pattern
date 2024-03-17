272db06ae29389aee9d73bb4486345929bc2151c
DeWuwo <767238116@qq.com>
2024-03-17 22:20:24 +0800

    feat: add func loading data to single file

--- script/file_move.py
+++ script/file_move.py
@@ -1,6 +1,6 @@
 import os
 
-from utils import Command
+from utils import Command, FileCSV
 from typing import List
 
 
@@ -14,3 +14,16 @@ class FileMove:
                 os.makedirs(dest_file)
             print(src_file, dest_file)
             Command.command_run(f'copy /Y {src_file} {dest_file}')
+
+    @classmethod
+    def file_csv_aggr(cls, src_paths: List[tuple], file: str, dest_path: str, out_name: str):
+        res = []
+        for src_path in src_paths:
+            src_file = os.path.join(src_path[1], file)
+            dest_file = os.path.join(dest_path)
+            if not os.path.exists(dest_file):
+                os.makedirs(dest_file)
+            data = {"project": src_path[0]}
+            data.update(FileCSV.read_dict_from_csv(src_file)[0])
+            res.append(data)
+        FileCSV.write_dict_to_csv(dest_path, f"{out_name}", res, "a", False)
